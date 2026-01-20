d6fae697e97cc5b08a213a2769bd8fbd48c51a5e
Amangoua Ange <amangouakacouange@gmail.com>
2026-01-20 10:33:21 +0000

    init TailwindCss

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+// https://astro.build/config
+export default defineConfig({
+  devToolbar: {
+    enabled: true,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
